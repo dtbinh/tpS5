43b27f787c289a0f1cfafbf8d5556fd1b329fedb
jhoukem <jhoukem@gmail.com>
2015-12-06 14:21:35 +0100

    pds tp8 cpt script

--- PDS/tp8/script_gnuplot.sh
+++ PDS/tp8/script_gnuplot.sh
@@ -23,12 +23,13 @@ rm -f $TIME_FILE && echo "# size_genome nb_threads time" > $TIME_FILE
 # un autre exemple de boucle
 for size in `awk 'BEGIN { for( i=100; i<=1000000000; i*=2 ) print i }'`; do
 	echo "Genome size : "$size" octet(s)" #affichage pour l'utilisateur de la taille courante
-	echo -n $size" " >> $TIME_FILE
+	
 	#creation du genome ici
 	./aleazard $size > $CMD_INPUT;
 	
 	for size2 in `awk 'BEGIN { for( i=1; i<=35; i*=2 ) print i }'`; do
 	    echo "Nb threads : "$size2
+	    echo -n $size" " >> $TIME_FILE
 	    echo -n $size2" " >> $TIME_FILE	
 	    $TIME_CMD $TIME_OPT $CMD $CMD_INPUT $size2 2>> $TIME_FILE
 	    done
